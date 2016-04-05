4214f925de35ad0943ed90c03cdcc29f91e2b602
Tong Kuo <contact@tong-kuo.me>
2016-04-05 13:20:44 +0800

    Update

---
+++ appcast-lab/appcast-lab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  appcast-lab
+//
+//  Created by Tong G. on 4/5/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ appcast-lab/appcast-lab/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  appcast-lab
+//
+//  Created by Tong G. on 4/5/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
